ebbe82fe4ad9f86cf3f59708436e8a16dae39fa8
hxy <dom@hxy.io>
2021-02-04 18:04:19 +0000

    Put ESC and ENTER on the thumbs
    Also CTRL, SHIFT for added convenience.

--- keymap.c
+++ keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,
     HOME_A,  HOME_R,  HOME_S,  HOME_T,  KC_G,    KC_M,    HOME_N,  HOME_E,  HOME_I,  HOME_O,
     KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
-                               XXXXXXX, THUMB_L, THUMB_R, XXXXXXX
+                               CORNR_L, THUMB_L, THUMB_R, CORNR_R
   ),
   [NAV] = LAYOUT(
     XXXXXXX, KC_HOME, KC_UP,   KC_END,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
--- keymap.h
+++ keymap.h
@@ -2,8 +2,10 @@
 
 #include QMK_KEYBOARD_H
 
-#define THUMB_L LT(NUM, KC_SPC)
-#define THUMB_R LT(NAV, KC_BSPC)
+#define THUMB_L MT(MOD_LCTL, KC_SPC)
+#define CORNR_L LT(NUM, KC_ESC)
+#define THUMB_R MT(MOD_LSFT, KC_ENT)
+#define CORNR_R LT(NAV, KC_BSPC)
 
 #define HOME_A LALT_T(KC_A)
 #define HOME_R LCTL_T(KC_R)
